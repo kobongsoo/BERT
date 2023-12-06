014bdc05c3b7bbaa0676eb8948b3519d214570ad
bong9431 <bong9431@empas.com>
2023-12-06 11:27:54 +0900

    delete kakao

--- FastAPI/kakao/mpowerai/pympower_ext/src/_mcrypt/stdafx.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <stdint.h>
-
-#if defined(__USE_MOCOCRYPTO_KISACRYPTO_MIXING)
-#if !defined(__USE_MOCOCRYPTO)
-#error "전처리 상수 __USE_MOCOCRYPTO_KISACRYPTO_MIXING를 선언하면 __USE_MOCOCRYPTO도 선언해야 합니다."
-#endif // !defined(__USE_MOCOCRYPTO)
-#endif // defined(__USE_MOCOCRYPTO_KISACRYPTO_MIXING)
